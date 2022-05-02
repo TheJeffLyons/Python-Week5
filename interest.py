8a69e676c1dc3c129ba892414683221199a460aa
Jeffrey Lyons <jeffreylyons@outlook.com>
2022-05-01 23:47:06 -0400

    commit 2

--- interest.py
+++ interest.py
@@ -1,38 +1,41 @@
-rate = input(
-    "\n Please enter the annualized interest rate or enter 'quit' to end the program: ")
+# double interest rate calculator using 72 rule
 
-if rate == 'quit':
-    exit()
+rate = "\n Please enter the annualized interest rate or enter 'quit' to end the program: "
 
-investment = input("\n Please enter initial investment amount: ")
+investment = "\n Please enter initial investment amount: "
 
-years = 0
-amount = int(investment)
+message = ""
+
+while True:
+
+    message = input(rate)
+
+    if message == 'quit':
+        break
+    else:
+        years = 72 / int(message)
+        message_two = input(investment)
+        print(
+            f"It will take {years} years for your investment of ${message_two} to double")
+
+            
+
+# rate = input(
+#     "\n Please enter the annualized interest rate or enter 'quit' to end the program: ")
+
+# if rate == 'quit':
+#     exit()
+
+# investment = input("\n Please enter initial investment amount: ")
+
+# years = 0
+# amount = int(investment)
 
 
-while amount < 2 * int(investment):
+# while amount < 2 * int(investment):
 
-    amount = amount + (amount * (int(rate)/100))
-    years += 1
-print(f"It will take {years} years for your investment to double")
+#     amount = amount + (amount * (int(rate)/100))
+#     years += 1
+# print(f"It will take {years} years for your investment to double")
 
 
-#double interest rate calculator using 72 rule
-
-# rate = "\n Please enter the annualized interest rate or enter 'quit' to end the program: "
-
-# investment = "\n Please enter initial investment amount: "
-
-# message = ""
-
-# while True:
-
-#     message = input(rate)
-
-#     if message == 'quit':
-#         break
-#     else:
-#         years = 72 / int(message)
-#         message_two = input(investment)
-#         print(
-#             f"It will take {years} years for your investment of ${message_two} to double")
